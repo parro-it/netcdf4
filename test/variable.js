80bd0c043241b32ba7c79f52e71bb5231731ae0a
Vasyl Moskalov <vasyl@h2i.sg>
2022-08-30 10:25:14 +0800

    Update tests. read/write slice for int64 does not work

--- test/variable.js
+++ test/variable.js
@@ -61,6 +61,16 @@ describe("Variable", function () {
     expect(res).to.be.equal(420);
   });
 
+  it("should rename an existing", function(){
+    fileold.root.variables.var1.name = "var2";
+    expect(fileold.root.variables).to.have.property("var2");
+    fileold.close();
+    fileold = new netcdf4.File(tempFileOldName, "w");
+    expect(fileold.root.variables).to.have.property("var2");
+
+  });
+
+
   it("should read a slice of existing", function () {
     var res = fileold.root.variables.var1.readSlice(0, 4)
     var results = Array.from(res);
@@ -120,6 +130,42 @@ describe("Variable", function () {
     expect(results).to.deep.equal([30,197,20.5,399]);
   });
 
+  it("should add new Variable whith set all parametrs", function(){
+    //    console.log(filenew.root.dimensions);
+        expect(filenew.root.variables).to.not.have.property("test_variable");
+        var newVar=filenew.root.addVariable('test_variable','byte',[filenew.root.dimensions.recNum.id]);
+        expect(newVar.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
+        expect(filenew.root.variables).to.have.property("test_variable");
+        expect(filenew.root.variables.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
+        newVar.endianness = "little";
+        newVar.checksummode = "fletcher32";
+        newVar.chunkmode = "chunked";
+        newVar.chunksizes = new Uint32Array([8]);
+        newVar.fillmode = true;
+        newVar.fillvalue = 8;
+        newVar.compressionshuffle = true;
+        newVar.compressiondeflate = true;
+        newVar.compressionlevel = 8;
+        newVar.addAttribute("len", "int", 42);
+        expect(newVar.attributes).to.have.property("len");
+        filenew.close();
+      
+        filenew = new netcdf4.File(tempFileNewName, "r");
+        expect(filenew.root.variables).to.have.property("test_variable");
+        expect(filenew.root.variables.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
+        newVar = filenew.root.variables.test_variable;
+        expect(newVar.endianness).to.equal("little");
+        expect(newVar.checksummode).to.equal("fletcher32");
+        expect(newVar.chunkmode).to.equal("chunked");
+        expect(newVar.chunksizes).to.deep.equal([8]);
+        expect(newVar.fillmode).to.equal(true);
+        expect(newVar.compressionshuffle).to.equal(true);
+        expect(newVar.compressiondeflate).to.equal(true);
+        expect(newVar.compressionlevel).to.equal(8);
+        expect(newVar.attributes).to.have.property("len");
+      });
+    
+
   const arrTypes={
     "byte":[Int8Array,Number],
     "short":[Int16Array,Number],
@@ -156,61 +202,51 @@ describe("Variable", function () {
         fd2.close();
       }
     )
+    it(`should read/write slice ${type} variable (${file}) `,
+      function() {
+        const [fd,path]=file==='netcdf3'?[fileold,tempFileOldName]:[filenew,tempFileNewName];
+        const dim=file==='netcdf3'?"dim1":"recNum"
+        expect(methods).to.be.not.empty;
+        expect(fd.root.variables).to.not.have.property("test_variable");
+        newVar=fd.root.addVariable('test_variable',type,[dim]);
+        newVar.writeSlice(0, 4,new methods[0](values))
+        expect(Array.from(fd.root.variables.test_variable.readSlice(0,4))).to.deep.almost.equal(values);
+        const fd2=new netcdf4.File(path,'r');
+        expect(fd2.root.variables).to.have.property("test_variable");
+        expect(Array.from(fd.root.variables.test_variable.readSlice(0,4))).to.deep.almost.equal(values);
+      }
+    )
+    it(`should read/write strided slice ${type} variable (${file}) `,
+      function() {
+        const [fd,path]=file==='netcdf3'?[fileold,tempFileOldName]:[filenew,tempFileNewName];
+        const dim=file==='netcdf3'?"dim1":"recNum"
+        expect(methods).to.be.not.empty;
+        expect(fd.root.variables).to.not.have.property("test_variable");
+        newVar=fd.root.addVariable('test_variable',type,[dim]);
+        newVar.writeStridedSlice(0, 2,2,new methods[0]([values[0],values[2]]))
+        expect(Array.from(fd.root.variables.test_variable.readStridedSlice(0,2,2))).to.deep.almost.equal([values[0],values[2]]);
+        const fd2=new netcdf4.File(path,'r');
+        expect(fd2.root.variables).to.have.property("test_variable");
+        expect(Array.from(fd.root.variables.test_variable.readStridedSlice(0,2,2))).to.deep.almost.equal([values[0],values[2]]);
+      }
+    )    
   }
 
   const testSuiteOld=[
-    ['byte',10,[10,20,30]],
-    ['short',1024,[20,512,333]],
+    ['byte',10,[10,20,30,40]],
+    ['short',1024,[20,512,333,1024]],
     ['int',100000,[0,-200,3000,555666]],
     ['float',153.2,[-12,33,55.5,106.2]],
-    ['double',153.2,[-12,33,55.5,106.2]],,
-    ['ubyte',10,[10,20,30]],
-    ['ushort',1024,[20,512,333]],
+    ['double',153.2,[-12,33,55.5,106.2]],
+    ['ubyte',10,[10,20,30,40]],
+    ['ushort',1024,[20,512,333,1024]],
     ['uint',100000,[0,200,3000,555666]]
   ];
   if (process.versions.node.split(".")[0]>=10) {
-    testSuiteOld.push(['uint64',1024,[20,512,333]]);
-    testSuiteOld.push(['int64',100000,[0,200,3000,555666]]);
+    testSuiteOld.push(['uint64',1024,[20n,512n,333n,77788889n]]);
+    testSuiteOld.push(['int64',100000,[0n,200n,3000n,555666n]]);
   };
   testSuiteOld.forEach(v=>testFunc('hdf5',v[0],v[1],v[2]));
 
-  it("name", function(){
-    fileold.root.variables.var1.name = "var2";
-    expect(fileold.root.variables).to.have.property("var2");
-  });
 
-  it("should add new Variable whith set all parametrs", function(){
-//    console.log(filenew.root.dimensions);
-    expect(filenew.root.variables).to.not.have.property("test_variable");
-    var newVar=filenew.root.addVariable('test_variable','byte',[filenew.root.dimensions.recNum.id]);
-    expect(newVar.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
-    expect(filenew.root.variables).to.have.property("test_variable");
-    expect(filenew.root.variables.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
-    newVar.endianness = "little";
-    newVar.checksummode = "fletcher32";
-    newVar.chunkmode = "chunked";
-    newVar.chunksizes = new Uint32Array([8]);
-    newVar.fillmode = true;
-    newVar.fillvalue = 8;
-    newVar.compressionshuffle = true;
-    newVar.compressiondeflate = true;
-    newVar.compressionlevel = 8;
-    newVar.addAttribute("len", "int", 42);
-    expect(newVar.attributes).to.have.property("len");
-    filenew.close();
-  
-    filenew = new netcdf4.File(tempFileNewName, "r");
-    expect(filenew.root.variables).to.have.property("test_variable");
-    expect(filenew.root.variables.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
-    newVar = filenew.root.variables.test_variable;
-    expect(newVar.endianness).to.equal("little");
-    expect(newVar.checksummode).to.equal("fletcher32");
-    expect(newVar.chunkmode).to.equal("chunked");
-    expect(newVar.chunksizes).to.deep.equal([8]);
-    expect(newVar.fillmode).to.equal(true);
-    expect(newVar.compressionshuffle).to.equal(true);
-    expect(newVar.compressiondeflate).to.equal(true);
-    expect(newVar.compressionlevel).to.equal(8);
-    expect(newVar.attributes).to.have.property("len");
-  });
 });
