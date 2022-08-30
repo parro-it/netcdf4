291888be6ef94b226cf03cb234132c2423ad6f54
Vasyl Moskalov <vasyl@h2i.sg>
2022-08-30 21:52:38 +0800

    Update NC_FORMAT defintion Update nodejs.yml

--- src/netcdf4jstypes.h
+++ src/netcdf4jstypes.h
@@ -2,6 +2,7 @@
 #define NETCDF4JSTYPES_H
 
 #include <netcdf.h>
+#include <netcdf_meta.h>
 #include <node_version.h>
 
 #if NODE_MAJOR_VERSION > 9
@@ -12,14 +13,20 @@
 
 // Ubuntu 22.04 and Ubuntu 20.04 have a different name for NCZARR file type constant
 // So....
+#if NETCDF_VERSION_MINOR < 7
+#define NC_FORMATS(ID) ID < NC_FORMATX_UNDEFINED || ID > NC_FORMATX_DAP4 ?"unsupported":netcdf4js::format_names[ID]
+#endif
+
+#if NETCDF_VERSION_MINOR < 8 && NETCDF_VERSION_MINOR >7
 #ifdef NC_FORMATX_NCZARR
 #define NC_FORMATS(ID) ID < NC_FORMATX_UNDEFINED || ID > NC_FORMATX_NCZARR ?"unsupported":netcdf4js::format_names[ID]
 #else
-#ifdef NC_FORMATX_ZARR
 #define NC_FORMATS(ID) ID < NC_FORMATX_UNDEFINED || ID > NC_FORMATX_ZARR ?"unsupported":netcdf4js::format_names[ID]
-#else
-#define NC_FORMATS(ID) ID < NC_FORMATX_UNDEFINED || ID > NC_FORMATX_UDF1 ?"unsupported":netcdf4js::format_names[ID]
 #endif
+#endif
+
+#if NETCDF_VERSION_MINOR >= 8
+#define NC_FORMATS(ID) ID < NC_FORMATX_UNDEFINED || ID > NC_FORMATX_NCZARR ?"unsupported":netcdf4js::format_names[ID]
 #endif
 
 namespace netcdf4js {
